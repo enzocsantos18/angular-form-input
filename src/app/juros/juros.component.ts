91ed80a34a9bf705b043e913d3a09129c21b00e3
enzocsantos18 <enzocsantos18@gmail.com>
2022-03-18 19:55:56 -0300

    2 files modified, 2 added

--- src/app/juros/juros.component.ts
+++ src/app/juros/juros.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-juros',
   templateUrl: './juros.component.html',
-  styleUrls: ['./juros.component.css']
+  styleUrls: ['./juros.component.css'],
 })
 export class JurosComponent implements OnInit {
+  vp: number;
+  j: number;
+  m: number;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.vp = 100;
+    this.j = 2.5;
+    this.m = 3;
   }
 
+  ngOnInit() {}
+
+  getJuros(): number {
+    return this.vp * (1 + (this.j / 100) * this.m);
+  }
 }
